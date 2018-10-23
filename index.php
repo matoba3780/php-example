6aa4dc119b96d0a1a12111700ebbe88b4ec20e21
matoba3780 <44385860+matoba3780@users.noreply.github.com>
2018-10-23 11:22:19 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php 
   echo "<h1>Hi! Hello world!</h1>\n"; 
   echo "hostname: " . gethostname() . "<BR>\n";
+  echo "============= 0 =============== <BR>\n";
   echo "IP-Addr : " . gethostname() . "\n";
+  echo "============= 1 =============== <BR>\n";
+  echo "11111111 : " . gethostname() . "<BR>";
+  echo "============= 2 =============== <BR>\n";
   echo "IP-Addr : " . $_SERVER['SERVER_ADDR'] . "\n"; ;?>
